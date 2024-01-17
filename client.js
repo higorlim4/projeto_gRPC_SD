071258f18621226af43c75a59df8e5ff6119b5ba
Higor Vinícius <limacocielo@gmail.com>
2024-01-17 14:46:32 -0300

    Update client.js

--- client.js
+++ client.js
@@ -28,17 +28,14 @@ function checkAvailability() {
     rl.question('Qual é o nome do filme? ', (movieName) => {
         rl.question('Qual é o horário da sessão (hh:mm)? ', (sessionTime) => {
             rl.question('Qual é o número do assento? ', (seatNumber) => {
-                // Converte a string de horário para um objeto Date
                 const sessionDate = new Date(`1970-01-01T${sessionTime}`);
 
-                // Formata o objeto Date para o formato desejado
                 const formattedSessionTime = sessionDate.toLocaleTimeString('pt-BR', { timeStyle: 'short' });
 
-                // Cria o objeto de solicitação a ser enviado ao servidor
                 const request = {
                     movie_name: movieName,
                     session_time: formattedSessionTime,
-                    seat_number: seatNumber.toUpperCase() // Converte a string para maiúsculas
+                    seat_number: seatNumber.toUpperCase()
                 };
 
                 client.checkAvailability(request, (error, response) => {
@@ -60,5 +57,4 @@ function checkAvailability() {
     });
 }
 
-// Example
 checkAvailability();
